9dbedab6861841be81d612123acb013da6abfc43
RFuentes <00062217@uca.edu.sv>
2020-04-27 00:36:15 -0600

    t

--- src/main/java/com/uca/capas/alumno/controller/MainController.java
+++ src/main/java/com/uca/capas/alumno/controller/MainController.java
@@ -65,7 +65,7 @@ public class MainController {
 			listF.add("El campo Lugar de Nacimiento no puede ser mayor a 25 caracteres o menor a 1 caracter");
 		}
 		if(school.length() < 1 || school.length() > 100) {
-			listF.add("El campo Instituto o colegio de procedencia no puede ser mayor a 100 caracteres");
+			listF.add("El campo Instituto o colegio de procedencia no puede ser mayor a 100 caracteres o menor a 1 caracter");
 		}
 		if(phone.length() != 8) {
 			listF.add("El campo Telefono fijo tienen que ser 8 digitos");
